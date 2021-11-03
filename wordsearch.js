f22a4c78de1fbd0efdc3d6dcacb3d7053b974248
Dennis Law <denn15_law@live.ca>
2021-11-03 22:02:19 +0000

    implement working solution by debuggin and add 2 test cases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,47 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  //if statement to check if letters is an array
+  if (!Array.isArray(letters)) {
+    return false;
+  }
 
-module.exports = wordSearch
+  //if statement to check if letters is empty array
+  if (letters.length === 0) {
+    return false;
+  }
+
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalLetters = transpose(letters);
+  const verticalJoin = verticalLetters.map((ls) => ls.join(""));
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+const transpose = (matrix) => {
+  //code here
+  const newArray = [];
+
+  //create new array
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArray.push([]);
+  }
+
+  // push items to new array
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      newArray[j].push(matrix[i][j]);
+    }
+  }
+
+  return newArray;
+};
+
+console.log(wordSearch([], "FRANK"));
+
+module.exports = wordSearch;
